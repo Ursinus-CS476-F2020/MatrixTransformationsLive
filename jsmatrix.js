f7e0e2df742c9477442017940df3acb311906d14
Chris Tralie <chris.tralie@gmail.com>
2020-09-23 19:44:50 -0400

    Major overhaul of code

--- jsmatrix.js
+++ jsmatrix.js
@@ -67,11 +67,13 @@ function makePlane(parent) {
 }
 
 /**
- * 
- * @param {array} labels String labels for each plot
+ * Add the plots with the 3x3 grid of colored squares
+ * @param {list of string} labels The label for each plot
+ * @param {float} width Width of each plot in pixels
+ * @param {float} height Height of each plot in pixels
+ * @param {float} shapeSide Dimension of each square in pixels
  */
 function addSquaresRow(labels, width, height, shapeSide) {
-    //<td><svg id ="Ax" width="300", height="300", viewBox="-100 -100 300 300"></svg></td>
     let row = document.createElement("tr");
     let svgs = [];
     for (let i = 0; i < labels.length; i++) {
@@ -99,14 +101,130 @@ function addSquaresRow(labels, width, height, shapeSide) {
     }
     return row;
 }
-    
+
+/**
+ * Create a row-major unrolled array of text inputs in a table
+ * to represent matrix elements
+ * @param {dom element} domElem DOM element to which to add the table
+ * @param {boolean} homogenous Whether to use homogenous coordinates
+ */
+function createMatrixInput(domElem, homogenous) {
+    let table = document.createElement("table");
+    table.border = 1;
+    let elems = [];
+    let ncols = 2;
+    if (homogenous) {
+        ncols = 3;
+    }
+    for (let i = 0; i < 2; i++) {
+        let row = document.createElement("tr");
+        for (let j = 0; j < ncols; j++) {
+            let col = document.createElement("td");
+            let input = document.createElement("input");
+            input.size = 1;
+            input.type = "text";
+            if (i == j) {
+                input.value = "1";
+            }
+            else {
+                input.value = "0";
+            }
+            elems.push(input);
+            col.appendChild(input);
+            row.appendChild(col);
+        }
+        table.appendChild(row);
+    }
+    if (homogenous) {
+        let row = document.createElement("tr");
+        let vals = ["&nbsp0", "&nbsp0", "&nbsp1"];
+        for (let j = 0; j < 3; j++) {
+            let col = document.createElement("td");
+            col.innerHTML = vals[j];
+            row.appendChild(col);
+        }
+        table.appendChild(row);
+    }
+    domElem.appendChild(table);
+    return elems;
+}
+
+/**
+ * Convert a grid of text inputs into a glMatrix.mat3 object
+ * @param {list of dom elements} elems Text inputs
+ */
+function textToMatrix(elems) {
+    let m = glMatrix.mat3.create();
+    let ms = [];
+    for (let i = 0; i < elems.length; i++) {
+        ms.push(parseFloat(elems[i].value));
+    }
+    if (ms.length == 4) {
+        ms = [ms[0], ms[1], 0, ms[2], ms[3], 0];
+    }
+    for (let i = 0; i < ms.length; i++) {
+        m[i] = ms[i];
+    }
+    return m;
+}
+
+/**
+ * Copy glMatrix.mat3 values over to text input elements
+ * @param {glMatrix.mat3} m matrix
+ * @param {list of dom elements} elems Text inputs
+ */
+function matrixToText(m, elems) {
+    if (elems.length == 4) {
+        elems[0].value = "" + m[0];
+        elems[1].value = "" + m[1];
+        elems[2].value = "" + m[3];
+        elems[3].value = "" + m[4];
+    }
+    else {
+        for (let i = 0; i < elems.length; i++) {
+            elems[i].value = "" + m[i];
+        }
+    }
+}
+
+/**
+ * Convert a 3x3 homogenous matrix into svg format
+ * @param {glMatrix.mat3} m The matrix
+ * @param {float} sideLen The length of a side of a square.
+ *                        Scale the translation by this amount
+ */
+function mat3ToSVG(m, sideLen) {
+    let ret = [m[0], m[1], m[3], m[4], sideLen*m[2], sideLen*m[5]];
+    return ret;
+}
+
+function transform(label, As, delay, sideLen) {
+    let shape = d3.select("#shape_"+label);
+    shape.attr("transform", "matrix([1, 0, 0, 1, 0, 0])");
+    As.forEach(function(A, index) {
+        shape.transition().delay(delay*(index+1))
+        .attr("transform", "matrix("+mat3ToSVG(A, sideLen)+")");
+    });
+}
 
 /**
  * Add the matrix widgets to a particular div element
+ * for Ax, Bx, A(Bx), and (AB)x
  * @param {string} id ID of parent element
  * @param {boolean} homogenous Whether to use homogenous coordinates
+ * @param {float} width Width of each transformation plot in pixels
+ * @param {float} height Height of each transformation plot in pixels
+ * @param {float} shapeSide Dimension of each square in pixels in the transformation plots
+ * @param {glMatrix.mat3} AInit Initial A matrix
+ * @param {glMatrix.mat3} BInit Initial B matrix
  */
-function addMatrixWidgets(id, homogenous) {
+function add2CompositionMatrixWidgets(id, homogenous, width, height, sideLen, AInit, BInit) {
+    if (AInit === undefined) {
+        AInit = glMatrix.mat3.create();
+    }
+    if (BInit === undefined) {
+        BInit = glMatrix.mat3.create();
+    }
     let labels = ["Ax", "Bx", "A(Bx)", "(AB)x"];
     let parent = document.getElementById(id);
     let table = document.createElement("table");
@@ -118,69 +236,39 @@ function addMatrixWidgets(id, homogenous) {
     table.appendChild(res.row);
     let buttons = res.buttons;
     // Third row with colorful squares
-    res = addSquaresRow(labels, 300, 300, 60);
+    res = addSquaresRow(labels, width, height, sideLen);
     table.appendChild(res);
-}
-
-
-
-
-
-
-
-
-var eye = [1, 0, 0, 1, 0, 0];
-
-function transform(index, As, delay) {
-    let shape = d3.select("#shape_"+index);
-    shape.attr("transform", "matrix("+eye+")");
-    As.forEach(function(A, index) {
-        shape.transition().delay(delay*(index+1))
-        .attr("transform", "matrix("+A+")");
-    });
-}
-
-/*for (var i = 0; i < 3; i++) {
-    transform(i, eye, eye, 0)
-}*/
-
-function getMatrices() {
-    var A = [];
-    var B = [];
-    // Column major!!
-    for (var j = 1; j <= 2; j++) {
-        for (var i = 1; i <= 2; i++) {
-            A.push(document.getElementById("a"+i+""+j).value);
-            B.push(document.getElementById("b"+i+""+j).value);
-        }
-        
+    // Add two matrix inputs
+    let matrixRow = document.createElement("tr");
+    let col = document.createElement("td");
+    let AInputs = createMatrixInput(col, homogenous);
+    matrixToText(AInit, AInputs);
+    matrixRow.appendChild(col);
+    col = document.createElement("td");
+    let BInputs = createMatrixInput(col, homogenous);
+    matrixToText(BInit, BInputs);
+    matrixRow.appendChild(col);
+    table.appendChild(matrixRow);
+    buttons["Ax"].onclick = function() {
+        let A = textToMatrix(AInputs);
+        transform(0, [A], 1000, sideLen);
     }
-    for (var i = 0; i < 2; i++) {
-        A.push(0); B.push(0);
+    buttons["Bx"].onclick = function() {
+        let B = textToMatrix(BInputs);
+        transform(1, [B], 1000, sideLen);
     }
-    var BA = [0, 0, 0, 0, 0, 0];
-    for (var j = 0; j < 2; j++) {
-        for (var i = 0; i < 2; i++) {
-            for (var k = 0; k < 2; k++) {
-                // ba_ij = \sum_k b_ik * a_kj
-                BA[j*2+i] += B[k*2+i]*A[j*2+k];
-            }
-        }
+    buttons["A(Bx)"].onclick = function() {
+        let A = textToMatrix(AInputs);
+        let B = textToMatrix(BInputs);
+        let BA = glMatrix.mat3.create();
+        glMatrix.mat3.multiply(BA, B, A);
+        transform(2, [A, BA], 1000, sideLen);
     }
-    return {'A':A, 'B':B, 'BA':BA};
-}
-
-function showA() {
-    res = getMatrices();
-    transform(0, [res['A']], 1000);
-}
-
-function showAB1() {
-    res = getMatrices();
-    transform(1, [res['A'], res['BA']], 1000)
-}
-
-function showAB2() {
-    res = getMatrices();
-    transform(2, [res['BA']], 1000);
+    buttons["(AB)x"].onclick = function() {
+        let A = textToMatrix(AInputs);
+        let B = textToMatrix(BInputs);
+        let BA = glMatrix.mat3.create();
+        glMatrix.mat3.multiply(BA, B, A);
+        transform(3, [BA], 1000, sideLen);
+    }
 }
